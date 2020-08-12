c65936cb0a0bc0681a43b3e9d389793d405143d3
N Young <60322086+nai-young@users.noreply.github.com>
2020-08-12 19:59:57 +0100

    routes express

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,6 +1,9 @@
+import User from '../models/user.model'
 import express from 'express'
 const router = express.Router()
 
 router.get('/users/createadmin', async (req, res) => {
-  const 
+  const user = new User({
+    name: ''
+  })
 })
